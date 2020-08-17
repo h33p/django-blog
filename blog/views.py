c9768e08a8906586feb9ae6868a44048211fec1c
Aurimas Blažulionis <0x60@pm.me>
2020-08-17 16:25:29 +0300

    Add blog post pagination

--- blog/views.py
+++ blog/views.py
@@ -11,8 +11,15 @@ class IndexView(generic.TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        posts = BlogPost.objects.order_by('-pub_date')[:(POSTS_PER_PAGE + 1)]
-        context['post_list'] = posts[:POSTS_PER_PAGE]
+        posts = BlogPost.objects.order_by('-pub_date')
+
+        paginator = Paginator(posts, POSTS_PER_PAGE)
+        page_number = self.request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
+        context['paginator'] = paginator
+        context['page_obj'] = page_obj
+
         context['categories'] = Category.objects.order_by('name')
         return context
 
@@ -29,9 +36,14 @@ class CategoryView(generic.DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         object = self.get_object()
-        print(object)
-        print(object.id)
-        posts = BlogPost.objects.filter(categories__id=object.id).order_by('-pub_date')[:(POSTS_PER_PAGE + 1)]
-        context['post_list'] = posts[:POSTS_PER_PAGE]
+        posts = BlogPost.objects.filter(categories__id=object.id).order_by('-pub_date')
+
+        paginator = Paginator(posts, POSTS_PER_PAGE)
+        page_number = self.request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
+        context['paginator'] = paginator
+        context['page_obj'] = page_obj
+
         context['categories'] = Category.objects.order_by('name')
         return context
